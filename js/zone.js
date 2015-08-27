3df79831612cd46a66cd9c481a4a693e9a1752cc
Matthew Steinwachs <matt.steinwachs@gmail.com>
2015-08-26 17:32:31 -0700

    Made the card image popup in mojosto bigger.

--- js/zone.js
+++ js/zone.js
@@ -2,11 +2,21 @@ var Zone;
 
 Zone = (function(){
   function Zone(options){
-    this.editable = options.editable || true;
+    this.cards_obscured = options.cards_obscured || false;
     this.cards = options.cards || {};
+  
+    this.$element = options.$element || null;
   };
 
-  Zone.prototype.initContextMenu = function(){ 
+  Zone.prototype.initContextMenuForCards = function(){ 
+    return null;
+  };
+
+  Zone.prototype.build = function(){
+    return null;
+  };
+
+  Zone.prototype.initZoneEvents = function(){
     return null;
   };
 
